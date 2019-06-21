046337f28e787ccb2052f63abd934a2f3b5e1961
Evan <skcy@vip.qq.com>
2019-06-21 14:31:18 +0800

    initial repo

---
+++ test/unit/specs/services/workers/heartbeat_worker.ts
@@ -0,0 +1,60 @@
+/*!
+ * This file is part of `services` module. 
+ * 
+ * Authors:
+ *      jason <jasonsoop@gmail.com>
+ * 
+ * Licensed under the MIT License.
+ * Copyright (C) 2010-2017 Uxmid Inc. All rights reserved. 
+ */
+
+import { assert } from "chai";
+import uxmid from "dist/uxmid";
+import Type = uxmid.Type;
+import IWorker = uxmid.IWorker;
+import WorkerBase = uxmid.WorkerBase;
+import WorkerStateChangedEventArgs = uxmid.WorkerStateChangedEventArgs;
+
+export default class HeartbeatWorker extends WorkerBase
+{
+    public constructor()
+    {
+        super("heartbeat");
+    }
+    
+    /**
+     * 当工作器启动时调用。
+     * @protected
+     * @override
+     * @param  {Array<string>} ...args
+     * @returns void
+     */
+    protected onStart(...args: Array<string>): void
+    {
+        // todo
+    }
+    
+    /**
+     * 当工作器停止时调用。
+     * @protected
+     * @override
+     * @param  {Array<string>} ...args
+     * @returns void
+     */
+    protected onStop(...args: Array<string>): void
+    {
+        // todo
+    }
+
+    /**
+     * 当工作器状态发生改变时调用。
+     * @protected
+     * @override
+     * @param  {WorkerStateChangedEventArgs} args 事件参数。
+     * @returns void
+     */
+    protected onStateChanged(args: WorkerStateChangedEventArgs): void
+    {
+        // todo
+    }
+}
