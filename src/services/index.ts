1b61eba818bc301517fec5d525989b26edf51f3f
Evan <skcy@vip.qq.com>
2019-06-26 16:56:05 +0800

    * Update Uxmid to UXmid.

--- src/services/index.ts
+++ src/services/index.ts
@@ -6,7 +6,7 @@
  * @Date:  2019-06-25 16:05:55
  * @version: 1.0.0
  * Licensed under the MIT License.
- * Copyright (C) 2010-present Uxmid Inc. All rights reserved. 
+ * Copyright (C) 2010-present UXmid Inc. All rights reserved. 
  */
 
 export { default as Injectable } from "./injectable";
